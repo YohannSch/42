e9a6bdc3211b20a5c1a28b64bf464e837e147d8b
YohannSch <yohannscheup@gmail.com>
2025-09-05 21:55:17 +0200

    test

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yscheupl <yscheupl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 13:56:04 by yscheupl          #+#    #+#             */
-/*   Updated: 2025/08/27 18:26:23 by yscheupl         ###   ########.fr       */
+/*   Updated: 2025/09/05 21:52:21 by yscheupl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	res = malloc(sizeof (char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!res)
 		return (NULL);
 	while (s1[i])
@@ -38,13 +38,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (res);
 }
 
-char	*fill_the_line(char *retour, char *temp)
+// Slight change: only one argument, returns up to '\n' or end
+char	*fill_the_line(char *temp)
 {
-	int		len;
+	int		len = 0;
 	char	*res;
-	
-	len = ft_strlen(temp) - ft_strlen(retour);
-	res = ft_substr(temp, 0, len);
+
+	if (!temp)
+		return (NULL);
+	while (temp[len] && temp[len] != '\n')
+		len++;
+	res = ft_substr(temp, 0, len + (temp[len] == '\n' ? 1 : 0)); // include \n if present
 	return (res);
 }
 
@@ -52,53 +56,69 @@ char	*getting_the_line(int fd, char *buffer, char *temp)
 {
 	int		flag;
 	char	*res;
-	char *i;
-	
+	char	*newline_pos;
+
 	flag = 1;
-	if(ft_strchr(temp, '\n') != NULL)
-		flag = -2;
-	while (flag > 0)
+	res = NULL;
+
+	// Check if temp already contains a newline
+	newline_pos = ft_strchr(temp, '\n');
+	while (flag > 0 && !newline_pos)
 	{
 		flag = read(fd, buffer, BUFFER_SIZE);
-		temp = ft_strjoin(temp, buffer);
-		if(ft_strchr(temp, '\n') != NULL)
-		{
-			flag = -2;
-			break;
-		}
 		if (flag == -1)
 		{
+			free(temp);
 			return (NULL);
 		}
-		if (flag < BUFFER_SIZE && flag >= 0)
-		{
-		res = ft_strdup(temp);
-		return (res);
-		}
+		buffer[flag] = '\0';
+		char *joined = ft_strjoin(temp, buffer);
+		free(temp);
+		temp = joined;
+		newline_pos = ft_strchr(temp, '\n');
 	}
-	if (flag == -2)
+
+	if (newline_pos) // found newline, normal case
 	{
-		res = fill_the_line(ft_strchr(temp, '\n') + 1, temp);
-		strcpy(buffer, ft_strchr(temp, '\n') + 1);
+		res = fill_the_line(temp);
+		// copy leftover to buffer
+		int i = 0;
+		newline_pos++;
+		while (*newline_pos)
+			buffer[i++] = *newline_pos++;
+		buffer[i] = '\0';
+		free(temp);
 		return (res);
 	}
-	// strcpy(buffer, ft_strchr(temp, '\n') + 1);
-	return (res);
+	else if (*temp) // EOF but temp has leftover data
+	{
+		res = ft_strdup(temp);
+		buffer[0] = '\0'; // clear buffer for next call
+		free(temp);
+		return (res);
+	}
+	free(temp);
+	return (NULL);
 }
 
 char *get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE];
+	static char	buffer[BUFFER_SIZE + 1] = {0};
 	char	*temp;
-	char * res;
-	
-	temp = malloc(100000);
-	res = malloc(100000);
+	char	*res;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+
+	temp = ft_strdup(buffer);
 	if (!temp)
 		return (NULL);
-	if (!res)
-		return (NULL);
-	temp = ft_strdup(buffer);
+
 	res = getting_the_line(fd, buffer, temp);
+	if (!res || !*res)
+	{
+		free(res);
+		return (NULL);
+	}
 	return (res);
 }
