5be0253ee3495fb121e0f2d915214e92e9e208f6
Yohann Scheuplein <yscheupl@f4r2s7.clusters.42paris.fr>
2025-10-20 15:50:21 +0200

    structure de p_s

---
+++ push_swap/push_swap.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yscheupl <yscheupl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 15:17:38 by yscheupl          #+#    #+#             */
+/*   Updated: 2025/10/20 15:24:02 by yscheupl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<push_swap.h>
+
+void push_swap(int argc, char **argv)
+{
+	// creation du tableau
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+		push_swap(argc, argv)
+	else
+		ft_putendl_fd("Error", 1)
+	return(0)
+}
